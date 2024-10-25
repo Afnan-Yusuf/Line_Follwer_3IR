692d8d74ff6fb5dd1604e1e75be6e26760ec451b
Afnan-Yusuf <afnanyusufpp@gmail.com>
2024-10-25 21:42:17 +0530

    for nit

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <Arduino.h>
 #include <Ramp.h>
-#define motlf 18 // motor pins
-#define motlb 5
-#define motrf 19
-#define motrb 12
+#define motlf 13 // motor pins
+#define motlb 12
+#define motrf 25
+#define motrb 33
 // #define _DEBUG_
 
 const int freqm = 5000; // motor pwm frequency
@@ -13,23 +13,29 @@ const int mlfc = 4;     // Motor Left Forward Channel (mlfc)
 const int mlbc = 6;     //
 const int mpwmr = 8;    // motor pwm resolution(8bit)
 
-int turnspeed = 170;
+int turnspeed = 150;
 int threshold = 200;
-int basespeed = 210;
+int basespeed = 190;
 
 bool oncenter = false;
 bool onleft = false;
 bool onright = false;
-int leftsensor = 25;
-int rightsensor = 27;
-int centersensor = 26;
+int leftsensor = 17;
+int rightsensor = 34;
+int centersensor = 39;
+int center1sensor = 18;
+int farleftsensor = 4;
+int farrightsensor = 32;
 int csval = 0;
 int lsval = 0;
 int rsval = 0;
+int error = 0;
 
 void fd();
 void rt();
 void lt();
+
+const int irPins[8] = {4, 16, 17, 18, 39, 34, 35, 32}; // Adjust according to your setup
 
 void setup()
 {
@@ -46,16 +52,53 @@ void setup()
   ledcWrite(mrbc, 0);
   ledcWrite(mlfc, 0);
   ledcWrite(mlbc, 0);
+  pinMode(centersensor, INPUT);
+  pinMode(leftsensor, INPUT);
+  pinMode(rightsensor, INPUT);
+  pinMode(center1sensor, INPUT);
+  pinMode(farleftsensor, INPUT);
+  pinMode(farrightsensor, INPUT);
 
 }
 void loop()
 {
-  csval = analogRead(centersensor);
-  lsval = analogRead(leftsensor);
-  rsval = analogRead(rightsensor);
-  csval > threshold ? oncenter = true : oncenter = false;
-  lsval > threshold ? onleft = true : onleft = false;
-  rsval > threshold ? onright = true : onright = false;
+  if(digitalRead(centersensor) == HIGH && digitalRead(center1sensor) == HIGH){
+    oncenter = true;
+    error = 0;
+  }else{
+    oncenter = false;
+  }
+
+
+  if(digitalRead(leftsensor) == HIGH){
+    onleft = true;
+    error = -1;
+  }else{
+    onleft = false;
+  }
+
+  if(digitalRead(rightsensor) == HIGH){
+    onright = true;
+    error = 1;
+  }else{
+    onright = false;
+  }
+
+  if(digitalRead(farleftsensor) == HIGH){
+    onleft = true;
+    error = -1;
+  }else{
+    onleft = false;
+  }
+
+  if(digitalRead(farrightsensor) == HIGH){
+    onright = true;
+    error = 1;
+  }else{
+    onright = false;
+  }
+
+
   if (oncenter == true && onleft == false && onright == false)
   {
     digitalWrite(LED_BUILTIN, HIGH);
